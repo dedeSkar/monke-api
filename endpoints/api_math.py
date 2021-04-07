b198f06984867d9b100fa8e15f3c5ac3c19b4a94
Mantas <51176389+dedeSkar@users.noreply.github.com>
2021-04-07 17:01:13 +0300

    Feature: is_number_prime

--- endpoints/api_math.py
+++ endpoints/api_math.py
@@ -9,3 +9,22 @@ router = APIRouter()
 def get_is_even(number: int, json: bool = False):
     is_even = not (number & 1 == 1)
     return magic(response_value=is_even, json=json)
+
+@router.get("/is_prime/{number}", name="is this number based? or nonprimelet")
+def is_number_prime(number :int, json :bool = False):
+    div_count = 0
+    if number == 2:
+        return True
+    elif number == 1:
+        return False
+    else:
+        for i in range(1, (round(math.sqrt(number))+1)):
+            if number % i == 0: 
+                div_count += 1
+            if div_count > 1: 
+                break
+        if div_count == 1:
+            is_prime = True
+        else: 
+            is_prime = False
+    return magic(response_value=is_prime, json=json)
