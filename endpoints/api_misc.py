d25463cfe3e0555e1ad2ddada35bdc656ef05534
Mantas <51176389+dedeSkar@users.noreply.github.com>
2021-04-07 17:08:55 +0300

    Update api_misc.py

--- endpoints/api_misc.py
+++ endpoints/api_misc.py
@@ -8,3 +8,8 @@ router = APIRouter()
 @router.get("/up", name="does monke-api work?")
 def get_up(json: bool = False):
     return magic(response_value=True, json=json)
+
+@router.get("/should_you_live_in_lithuania")
+def should_you_live_in_lithuania(json: bool = False):
+    #todo update to True, when the situation changes
+    return magic(response_value-False, json=json)
