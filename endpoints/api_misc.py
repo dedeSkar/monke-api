413f3b2b87c3ef6df948c485ee122415d3c0b57f
pauliusbaulius <spam@paulius.xyz>
2021-04-07 15:52:53 +0200

    headers/ ip/ endpoints

--- endpoints/api_misc.py
+++ endpoints/api_misc.py
@@ -1,7 +1,15 @@
 from utils import magic
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 router = APIRouter()
 
 
+@router.get("/ip", name="your ip address")
+def get_ip(request: Request, json: bool = False):
+    return magic(response_value=request.client.host, json=json)
+
+
+@router.get("/headers", name="your request headers")
+def get_ip(request: Request):
+    return request.headers
